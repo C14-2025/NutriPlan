31f5f857673d61f8f3770091151064667bf08303
jovsxf <gioemanuely@gmail.com>
2025-09-01 20:54:18 -0300

    first commit

---
+++ src/main/java/com/nutriplan/model/alimento.java
@@ -0,0 +1,68 @@
+package com.nutriplan.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Alimento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    // Valores por 100g
+    private Double calorias;
+    private Double carboidratos;
+    private Double proteinas;
+    private Double gorduras;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(Double calorias) {
+        this.calorias = calorias;
+    }
+
+    public Double getCarboidratos() {
+        return carboidratos;
+    }
+
+    public void setCarboidratos(Double carboidratos) {
+        this.carboidratos = carboidratos;
+    }
+
+    public Double getProteinas() {
+        return proteinas;
+    }
+
+    public void setProteinas(Double proteinas) {
+        this.proteinas = proteinas;
+    }
+
+    public Double getGorduras() {
+        return gorduras;
+    }
+
+    public void setGorduras(Double gorduras) {
+        this.gorduras = gorduras;
+    }
+}
---
+++ src/main/java/com/nutriplan/model/refeicao.java
@@ -0,0 +1,64 @@
+package com.nutriplan.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Refeicao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tipo; // Café da manhã, Almoço, Jantar, Lanche
+    private LocalDateTime dataHora;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "refeicao", cascade = CascadeType.ALL)
+    private List<ItemRefeicao> itens;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public List<ItemRefeicao> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemRefeicao> itens) {
+        this.itens = itens;
+    }
+}
