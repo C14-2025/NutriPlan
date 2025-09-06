27eadee6ded7c5644b712556ac11986646fd3223
natalia-batista <batistanatp@gmail.com>
2025-09-06 17:13:44 -0300

    feat- alimento repository

---
+++ src/main/java/br/inatel/nutriPlan/repositories/AlimentoRepository.java
@@ -0,0 +1,7 @@
+package br.inatel.nutriPlan.repositories;
+
+import br.inatel.nutriPlan.models.Alimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlimentoRepository extends JpaRepository<Alimento, Integer> {
+}
