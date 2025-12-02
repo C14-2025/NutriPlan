b5cd1f30ffbb1fd5113bfdbdd1102f672b5b7734
jovsxf <gioemanuely@gmail.com>
2025-12-02 01:09:47 -0300

    testesHistorico funciona

--- cypress/e2e/testesHistorico.cy.js
+++ cypress/e2e/testesHistorico.cy.js
@@ -1,91 +1,73 @@
 describe("MealHistory - Histórico de Refeições", () => {
+  const mealsMock = [
+    {
+      id: '1',
+      name: 'Refeição',
+      date: '2025-12-01',
+      usuario: { id: 1 }
+    }
+  ];
+
+  const alimentosMock = [
+    { id: 10, nome: 'Arroz branco cozido', calorias: 130, proteinas: 2.4, carboidratos: 28.2, gorduras: 0.3 }
+  ];
+
   beforeEach(() => {
-    // simula usuário logado
+    
     window.localStorage.setItem('usuarioId', '1');
     window.localStorage.setItem('usuarioNome', 'Teste');
 
-    // mock da rota principal
-    cy.intercept("GET", "/refeicao/usuario/1", {
+    
+    cy.intercept('GET', '/alimento', { statusCode: 200, body: alimentosMock }).as('getFoods');
+    cy.intercept('GET', '/refeicao/usuario/1', { statusCode: 200, body: mealsMock }).as('getMeals');
+    cy.intercept('GET', '/refeicao/1/alimentos', {
       statusCode: 200,
-      body: [
-        {
-          id: 1,
-          tipo: "Almoço",
-          dataHora: "2025-12-01T12:00:00",
-          alimentos: [
-            {
-              id: 10,
-              nome: "Arroz",
-              calorias: 130,
-              proteinas: 3,
-              carboidratos: 28,
-              gorduras: 1
-            }
-          ],
-          quantidadePorAlimento: {
-            10: 150
-          }
-        }
-      ]
-    }).as("getMeals");
+      body: [{ alimentoId: 10, quantidade: 150 }]
+    }).as('getMealFoods');
+    cy.intercept('DELETE', '/refeicao/1', { statusCode: 204 }).as('deleteMeal');
+    cy.intercept('PUT', '/refeicao/1', { statusCode: 200 }).as('updateMeal');
 
-    cy.visit("http://localhost:5173");
-    cy.wait("@getMeals");
-    cy.contains("Histórico").click();
+    cy.visit('http://localhost:5173');
+
+    cy.contains('Histórico').click();
+
   });
-
 
   it("Deve exibir o histórico da refeição corretamente", () => {
-    cy.contains("Almoço").should("exist");
+    cy.contains("Refeição").should("exist");
     cy.contains("Arroz").should("exist");
-
-    // verifica quantidade vinda do backend
     cy.contains("150").should("exist");
-
   });
-
 
   it("Deve filtrar por tipo de refeição", () => {
     cy.contains("Tipo de Refeição").click();
-    cy.contains("Almoço").click();
-
-    cy.contains("Almoço").should("exist");
+    cy.contains("Refeição").click();
+    cy.contains("Refeição").should("exist");
   });
-
 
   it("Deve filtrar por data", () => {
-    cy.get("input[type='date']").type("2025-12-01");
+    cy.get("input[type='date']").invoke('val', '2025-12-01').trigger('change');
+    cy.contains("Refeição").should("exist");
+  });
+
+  it("Deve editar uma refeição", () => {
+    cy.contains("Editar").click();
+    cy.get("[role='combobox']");
+    cy.contains("Almoço").click({ force: true });
+    cy.get("button").contains("Salvar").click({ force: true });
     cy.contains("Almoço").should("exist");
   });
 
-
-  it("Deve abrir o diálogo de edição da refeição", () => {
-    cy.contains("Almoço")
-      .parents("[data-meal-item]")
-      .find("button")
-      .contains("Editar")
-      .click();
-
-    cy.contains("Editar Refeição").should("exist");
-
-    cy.get("[role='combobox']").click();
-    cy.contains("Jantar").click();
+  it("Deve editar e excluir uma refeição", () => {
+    cy.contains("Editar").click();
+    cy.get("[role='combobox']");
+    cy.contains("Almoço").click({ force: true });
+    cy.get("button").contains("Salvar").click({ force: true });
+    cy.contains("Almoço").should("exist");
+    cy.contains("Excluir").click();
+    cy.on('window:confirm', () => true);
+    cy.wait('@deleteMeal');
+    cy.contains("Almoço").should("not.exist");
   });
 
-
-  it("Deve excluir uma refeição", () => {
-    cy.intercept("DELETE", "/refeicao/1", {
-      statusCode: 204
-    }).as("deleteMeal");
-
-    cy.contains("Almoço")
-      .parents("[data-meal-item]")
-      .find("button")
-      .contains("Excluir")
-      .click();
-
-    cy.on("window:confirm", () => true);
-
-    cy.wait("@deleteMeal");
-  });
 });
